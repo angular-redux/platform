372e94b15d73fddadf051f7e78222dfa5928a768
Chris Bond <chris.bond@rangle.io>
2016-07-24 13:36:04 -0400

    Better unit tests for ImmutableJS state composition

--- packages/form/source/functions/compose-reducers.test.ts
+++ packages/form/source/functions/compose-reducers.test.ts
@@ -1,6 +1,7 @@
 import { expect } from 'chai';
 
 import {
+  fromJS,
   List,
   Map,
   OrderedSet,
@@ -10,19 +11,59 @@ import {
 import { composeReducers } from './compose-reducers';
 
 describe('composeReducers', () => {
-  it('can compose multiple plain-object reducers and initial states', () => {
-    const r1 = (state = {a: 1}, action) => state;
-    const r2 = (state = {b: 1}, action) => state;
+  const compose = (s1, s2, s3) => {
+    const r1 = (state = s1, action) => state;
+    const r2 = (state = s2, action) => state;
 
-    const reducer = composeReducers<any>({c: 1}, r1, r2);
+    const reducer = composeReducers<any>(s3, r1, r2);
 
-    const composedState = reducer(undefined, {type: ''});
-
-    expect(composedState).not.to.be.undefined;
-    expect(composedState.a).to.eq(1)
-    expect(composedState.b).to.eq(1);
-    expect(composedState.c).to.eq(1);
+    return reducer(undefined, {type: ''});
+  }
+  it('can compose plain-object initial states', () => {
+    const state = compose({a: 1}, {b: 1}, {c: 1});
+    expect(state).not.to.be.undefined;
+    expect(state).to.deep.equal({a: 1, b: 1, c: 1});
   });
 
+  it('can compose Immutable::Map initial states', () => {
+    const state = compose(fromJS({a: 1}), fromJS({b: 1}), fromJS({c: 1}));
+    expect(Map.isMap(state)).to.be.true;
 
+    const plain = state.toJS();
+    expect(plain).not.to.be.null;
+    expect(plain).to.deep.equal({a: 1, b: 1, c: 1});
+  });
+
+  it('can compose Immutable::Set initial states', () => {
+    const state = compose(Set.of(1, 2, 3), Set.of(4, 5, 6), Set.of());
+    expect(Set.isSet(state)).to.be.true;
+
+    const plain = state.toJS();
+    expect(plain).not.to.be.null;
+    expect(plain).to.deep.equal([1, 2, 3, 4, 5, 6]);
+  });
+
+  it('can compose Immutable::OrderedSet initial states', () => {
+    const state = compose(Set.of(3, 2, 1), Set.of(4, 6, 5), Set.of());
+    expect(Set.isSet(state)).to.be.true;
+
+    const plain = state.toJS();
+    expect(plain).not.to.be.null;
+    expect(plain).to.deep.equal([3, 2, 1, 4, 6, 5]);
+  });
+
+  it('can compose Immutable::List initial states', () => {
+    const state = compose(List.of('a', 'b'), List.of('c', 'd'), List.of());
+    expect(List.isList(state)).to.be.true;
+
+    const plain = state.toJS();
+    expect(plain).not.to.be.null;
+    expect(plain).to.deep.equal(['a', 'b', 'c', 'd']);
+  });
+
+  it('throws an exception when given a null initial state', () => {
+    expect(() => {
+      const state = compose(List.of(), List.of(), null);
+    }).to.throw();
+  });
 });
