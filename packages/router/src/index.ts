c3855792760b46c8e88e2ff80e1ede5c7284b89b
Dag Stuan <D.Stuan@gmail.com>
2016-09-09 09:38:04 +0200

    NgReduxRouterModule

--- packages/router/src/index.ts
+++ packages/router/src/index.ts
@@ -1,12 +1,17 @@
-export {
-  NgReduxRouter
-} from './router';
+import { NgModule } from '@angular/core';
+import { NgReduxRouter } from './router';
+import { RouterAction, routerReducer } from './reducer';
+import { UPDATE_LOCATION } from './actions';
+
+@NgModule({
+  providers: [ NgReduxRouter ]
+})
+class NgReduxRouterModule { }
 
 export {
+  NgReduxRouterModule,
+  NgReduxRouter,
+  RouterAction,
   routerReducer,
-  RouterAction
-} from './reducer';
-
-export {
   UPDATE_LOCATION
-} from './actions';
+};
