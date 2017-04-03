4675c43b0ffe35b43bc9029ed875199be1e14d02
Seth Davenport <SethDavenport@users.noreply.github.com>
2017-04-02 21:55:30 -0400

    Consolidating examples (#11)
    Maintaining a lot of example apps in each repo is becoming a time suck.  Going forward, angular-redux/example-app should serve as both comprehensive example and testbed for the other packages.

--- packages/router/examples/counter/index.ts
+++
@@ -1,4 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './containers/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule)
