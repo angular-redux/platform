ea94d5dc5a04dcb18feeb3b52546e0abe57bfd1a
James Salas <j@jamesbs.com>
2018-12-19 17:04:12 -0500

    fix: rxjs conflicting type signatures

--- packages/example-app/src/app/component.spec.ts
+++ packages/example-app/src/app/component.spec.ts
@@ -1,9 +1,19 @@
 import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './component';
+import {
+  platformBrowserDynamicTesting,
+  BrowserDynamicTestingModule,
+} from '@angular/platform-browser-dynamic/testing';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
+    TestBed.resetTestEnvironment();
+    TestBed.initTestEnvironment(
+      BrowserDynamicTestingModule,
+      platformBrowserDynamicTesting(),
+    );
+
     TestBed.configureTestingModule({
       declarations: [AppComponent],
       imports: [RouterTestingModule],
