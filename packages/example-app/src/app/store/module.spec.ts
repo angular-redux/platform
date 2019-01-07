a3c10fda3e3acc9360dc83e05a89cbf9813d70af
James Salas <j@jamesbs.com>
2019-01-07 16:50:30 -0500

    fix: issue with epicmiddleware not being called in mock test

--- packages/example-app/src/app/store/module.spec.ts
+++ packages/example-app/src/app/store/module.spec.ts
@@ -1,36 +1,35 @@
-import { DevToolsExtension, NgRedux } from '@angular-redux/store';
-import {
-  MockNgRedux,
-  NgReduxTestingModule,
-} from '@angular-redux/store/testing';
-import { async, TestBed } from '@angular/core/testing';
+import { async } from '@angular/core/testing';
+import { applyMiddleware, compose, createStore } from 'redux';
 import { RootEpics } from './epics';
 import { StoreModule } from './module';
 
 describe('Store Module', () => {
-  let mockNgRedux: NgRedux<any>;
-  let devTools: DevToolsExtension;
   let mockEpics: Partial<RootEpics>;
 
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [NgReduxTestingModule],
-    });
-
     mockEpics = {
       createEpics() {
         return [];
       },
     };
-    devTools = TestBed.get(DevToolsExtension);
-    mockNgRedux = MockNgRedux.getInstance();
   }));
 
   it('should configure the store when the module is loaded', async(() => {
-    const configureSpy = spyOn(MockNgRedux.getInstance(), 'configureStore');
-    const module = new StoreModule(
-      mockNgRedux,
-      devTools,
+    const mockNgRedux = {
+      configureStore(_: any, __: any, middleware: any = [], ___: any) {
+        createStore(
+          () => '',
+          compose(...middleware.map((m: any) => applyMiddleware(m))),
+        );
+      },
+    };
+    const configureSpy = spyOn(mockNgRedux, 'configureStore').and.callThrough();
+    const store = new StoreModule(
+      mockNgRedux as any,
+      {
+        enhancer: jasmine.createSpy(),
+        isEnabled: jasmine.createSpy(),
+      } as any,
       null as any,
       mockEpics as any,
     );
